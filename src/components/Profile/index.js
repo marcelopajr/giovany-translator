98e35e8dc335167e85acfbebcb7769b468643f50
Marcelo Pereira <marcelo96jr@gmail.com>
2021-03-11 11:33:02 -0300

    Iniciando componente Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Giovany Almeida</h1>
+    <h2>Translator</h2>
+    <p>
+      Translation services that aim helping clients to reach an international
+      audience.
+    </p>
+  </div>
+)
+
+export default Profile

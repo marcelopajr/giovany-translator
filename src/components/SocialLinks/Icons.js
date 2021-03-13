6328799d0372d6019602bca0f0995986a2707c27
Marcelo Pereira <marcelo96jr@gmail.com>
2021-03-13 20:27:08 -0300

    SocialLinks funcionando

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,11 @@
+import { Linkedin } from "@styled-icons/bootstrap/Linkedin"
+import { Profile } from "@styled-icons/icomoon/Profile"
+import { Email } from "@styled-icons/entypo/Email"
+
+const Icons = {
+  Linkedin,
+  Profile,
+  Email,
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+  {
+    label: "Linkedin",
+    url: "https://www.linkedin.com/in/giovany-almeida-1197691ba/",
+  },
+  {
+    label: "Profile",
+    url: "https://www.proz.com/translator/2655736",
+  },
+  {
+    label: "Email",
+    url: "mailto:giovanyalmeida.translator@gmail.com",
+  },
+]
+
+export default links

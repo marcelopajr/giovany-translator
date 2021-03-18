50286765f8998c29de60c650a985b9b90e6865ec
Marcelo Pereira <marcelo96jr@gmail.com>
2021-03-18 18:02:56 -0300

    Variáveis no CSS e Mudança de Tema

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Home } from "@styled-icons/boxicons-solid/Home"
 import { SearchAlt2 as Search } from "@styled-icons/boxicons-regular/SearchAlt2"
 import { UpArrowAlt as Arrow } from "@styled-icons/boxicons-regular/UpArrowAlt"
@@ -6,34 +6,51 @@ import { Bulb as Light } from "@styled-icons/boxicons-regular/Bulb"
 import { Grid } from "@styled-icons/boxicons-solid/Grid"
 import * as S from "./styled"
 
-const MenuBar = () => (
-  <S.MenuBarWrapper>
-    <S.MenuBarGroup>
-      <S.MenuBarLink to="/" title="Back to home">
-        <S.MenuBarItem>
-          <Home />
-        </S.MenuBarItem>
-      </S.MenuBarLink>
+const MenuBar = () => {
+  const [theme, setTheme] = useState(null)
 
-      <S.MenuBarLink to="/search/" title="Search">
-        <S.MenuBarItem>
-          <Search />
-        </S.MenuBarItem>
-      </S.MenuBarLink>
-    </S.MenuBarGroup>
+  const isDarkMode = theme === "dark"
 
-    <S.MenuBarGroup>
-      <S.MenuBarItem title="Change theme">
-        <Light />
-      </S.MenuBarItem>
-      <S.MenuBarItem title="Change view">
-        <Grid />
-      </S.MenuBarItem>
-      <S.MenuBarItem title="Back to top">
-        <Arrow />
-      </S.MenuBarItem>
-    </S.MenuBarGroup>
-  </S.MenuBarWrapper>
-)
+  useEffect(() => {
+    setTheme(window.__theme)
+    window.__onThemeChange = () => setTheme(window.__theme)
+  }, [])
+
+  return (
+    <S.MenuBarWrapper>
+      <S.MenuBarGroup>
+        <S.MenuBarLink to="/" title="Back to home">
+          <S.MenuBarItem>
+            <Home />
+          </S.MenuBarItem>
+        </S.MenuBarLink>
+
+        <S.MenuBarLink to="/search/" title="Search">
+          <S.MenuBarItem>
+            <Search />
+          </S.MenuBarItem>
+        </S.MenuBarLink>
+      </S.MenuBarGroup>
+
+      <S.MenuBarGroup>
+        <S.MenuBarItem
+          title="Change theme"
+          onClick={() => {
+            window.__setPreferredTheme(isDarkMode ? "light" : "dark")
+          }}
+          className={theme}
+        >
+          <Light />
+        </S.MenuBarItem>
+        <S.MenuBarItem title="Change view">
+          <Grid />
+        </S.MenuBarItem>
+        <S.MenuBarItem title="Back to top">
+          <Arrow />
+        </S.MenuBarItem>
+      </S.MenuBarGroup>
+    </S.MenuBarWrapper>
+  )
+}
 
 export default MenuBar
